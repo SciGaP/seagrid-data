2bfe5fa422fa653183729c32c26c1174c396a143
scnakandala <supun.nakandala@gmail.com>
2016-02-23 11:51:32 -0500

    adding login/logout logic

--- web/search.php
+++ web/search.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
     if(!isset($_SESSION['username'])){
         $home_url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . '/index.php';
         header('Location: ' . $home_url);
